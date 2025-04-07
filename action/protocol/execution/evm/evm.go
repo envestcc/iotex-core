0a10e260525aeb668cb63598afaa9b9672c97066
envestcc <chen1233216@hotmail.com>
2025-04-07 19:59:36 +0800

    use adapterwithlog

--- action/protocol/execution/evm/evm.go
+++ action/protocol/execution/evm/evm.go
@@ -289,7 +289,7 @@ func ExecuteContract(
 			}
 		}
 	}
-
+	stateDB = newAdapterWithLog(stateDB)
 	retval, depositGas, remainingGas, contractAddress, statusCode, err := executeInEVM(ctx, ps, stateDB)
 	if err != nil {
 		return nil, nil, err
